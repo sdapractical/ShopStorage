6cbdcf40aec6ac7eeffa89334448607dd731c6c4
Aimi <email@gmail.com>
2021-02-09 18:41:04 +0200

    create domain classes Category and Product

---
+++ src/main/java/domain/Product.java
@@ -0,0 +1,51 @@
+package domain;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private String manufacturer;
+    private boolean price;
+    private int quantity;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public boolean isPrice() {
+        return price;
+    }
+
+    public void setPrice(boolean price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
